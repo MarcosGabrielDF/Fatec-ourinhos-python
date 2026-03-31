8be27b99ca150e77ae903f7821dbbc044ead05c6
Marcos Gabriel Dias Fernandes <110645752+MarcosGabrielDF@users.noreply.github.com>
2026-03-30 22:04:49 -0300

    Add script to read a 4-digit number and count digits

--- Lista 3/ex2.py
+++ Lista 3/ex2.py
@@ -2,7 +2,6 @@
 2) Faça um script que leia um número de 4 dígitos e informe a quantidade de milhar, centena, dezena e
 unidade;
 """
-from os.path import join
 
 numero = input('Escreva um número de 4 digítos: ')
 
